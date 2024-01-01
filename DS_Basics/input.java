26df6e989f43c8e0a93677796467dc7e6be0859b
SubhamPC <helpingtechy@gmail.com>
2024-01-01 21:10:56 +0530

    1/24

---
+++ DS_Basics/input.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+class input{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String input = sc.next();
+        System.out.println(input);
+
+        //newline
+        sc.nextLine();
+        String name = sc.nextLine();
+        System.out.println(name);
+
+        // sc.nextInt();
+        int num = sc.nextInt();
+        System.out.println(num);
+        //float
+        float num1 = sc.nextFloat();
+
+    }
+}
---
+++ DS_Basics/typecaste.java
@@ -0,0 +1,11 @@
+public class typecaste {
+    public static void main(String[] args) {
+        float a = 1229.65f;
+        int b = (int) a;
+        System.out.println(b);
+        //char
+        char ch = 'a';
+        int c = (int) ch;
+        System.out.println(c);
+    } 
+}
